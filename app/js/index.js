0e13e935b8d60b2b9315927f1f1628596b07b0e5
Richard Ramos <info@richardramos.me>
2018-06-29 13:11:10 -0400

    Adding react to dapp

--- app/js/index.js
+++ app/js/index.js
@@ -4,3 +4,16 @@ import EmbarkJS from 'Embark/EmbarkJS';
 // e.g if you have a contract named SimpleStorage:
 //import SimpleStorage from 'Embark/contracts/SimpleStorage';
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+
+    render(){
+        return <div>
+            Hola Mundo
+            </div>;
+    }
+}
+
+ReactDOM.render(<App></App>, document.getElementById('app'));
